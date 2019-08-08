99e591145ab6f07fd730c7f3f91cf6c0aa3aec46
AnriTapel <anri.snep@gmail.com>
2019-08-08 22:16:42 +0300

    about me subpage completed

--- src/app/breeder-profile/about-me-profile-page/about-me-profile-page.component.ts
+++ src/app/breeder-profile/about-me-profile-page/about-me-profile-page.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeProfilePageComponent implements OnInit {
 
+  breederData: any;
+  currentClub: string = null;
+  curCertificates: [];
+  curPhoto: any;
+
   constructor() { }
 
   ngOnInit() {
+    this.breederData = {
+      aboutMe: null,
+      begining: null,
+      nurceryFeatures: null,
+      photo: null,
+      clubs: [],
+      certificates: []
+    }
+
+  }
+
+  addClub(): void{
+    this.breederData.clubs.push(this.currentClub);
+    this.currentClub = null;
+  }
+
+  deleteClub(index: number): void{
+    this.breederData.clubs.splice(index, 1);
+  }
+
+  saveChanges(){
+
   }
 
 }
