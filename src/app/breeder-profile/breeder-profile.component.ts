929f2ab55ab4497b3230a497c9f72d205f54b58c
AnriTapel <anri.snep@gmail.com>
2019-08-21 00:25:25 +0300

    profile menu for mobile

--- src/app/breeder-profile/breeder-profile.component.ts
+++ src/app/breeder-profile/breeder-profile.component.ts
@@ -8,19 +8,28 @@ import { AppService } from '../app.service';
 })
 export class BreederProfileComponent implements OnInit {
 
-  private curProfilePage: string;
+  private curProfilePage: any;
+  profileSubpages: any[] = [
+    {tag: 'about-nurcery', name: 'О питомнике'},
+    {tag: 'about-puppies', name: 'О щенках'},
+    {tag: 'puppies-parents', name: 'Родители щенков'},
+    {tag: 'about-me', name: 'О себе'},
+    {tag: 'add-puppy', name: 'Добавить щенка'}
+  ];
+  isMobileMenuVisible: boolean = false;
 
   constructor(private appService: AppService) { }
 
   ngOnInit() {
-    this.setCurProfilePage("about-nurcery");
+    this.setCurProfilePage(this.profileSubpages[0]);
   }
 
-  setCurProfilePage(page: string): void {
+  setCurProfilePage(page: any): void {
     this.curProfilePage = page;
+    this.isMobileMenuVisible = false;
   }
 
-  getCurProfilePage(): string {
+  getCurProfilePage(): any {
     return this.curProfilePage;
   }
 
