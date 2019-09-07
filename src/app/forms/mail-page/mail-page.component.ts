faa4018f02e9d0cdfcff03f21125343cee05c6c3
AnriTapel <anri.snep@gmail.com>
2019-09-07 14:36:07 +0300

    confirm again alert

--- src/app/forms/mail-page/mail-page.component.ts
+++ src/app/forms/mail-page/mail-page.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import { AppService } from '../../services/app-service/app.service';
 import { BreederControllerService } from '../..';
 import { NotificationBarService } from '../../services/nofitication-service/notification-bar.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     selector: 'app-mail-page',
@@ -13,11 +13,25 @@ export class MailPageComponent implements OnInit {
 
     mail: string;
 
-    constructor(private route: ActivatedRoute) {
+    constructor(private appService: AppService, private breederService: BreederControllerService,
+        private notificationService: NotificationBarService, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
         this.mail = this.route.snapshot.paramMap.get('mail');
+    }
+
+    confirmEmailAgain(): void{
+        this.breederService.confirmAgainUsingPOST().subscribe(
+            () => {
+                this.notificationService.setContext('Письмо успешно отправлено', true);
+                this.notificationService.setVisibility(true);
+            },
+            () => {
+                this.notificationService.setContext('Не удалось отправить письмо, попробуйте еще раз', false);
+                this.notificationService.setVisibility(true);
+            }
+        );
     }
 
 }
