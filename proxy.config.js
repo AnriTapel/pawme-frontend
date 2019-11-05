2a9064a4b5336150dc283757ecceacaf075eeb48
Anri Tapel <anri.snep@gmail.com>
2019-11-05 15:58:29 +0300

    breeder-landing-title structure edited + prod version

--- proxy.config.js
+++ proxy.config.js
@@ -1,16 +1,16 @@
 const PROXY_CONFIG = {
     "/api/*": {
-        "target": "https://dev.petman.co",
+        "target": "https://petman.co",
         "secure": true,
         "changeOrigin": true,
         "logLevel": "debug",
         "onProxyRes": function (proxyRes, req, res) {
             if (proxyRes.statusCode == 302)
-                proxyRes.headers.location = proxyRes.headers.location.replace('https://dev.petman.co', '');
+                proxyRes.headers.location = proxyRes.headers.location.replace('https://petman.co', '');
         },
     },
     "/img/*": {
-        "target": "https://dev.petman.co",
+        "target": "https://petman.co",
         "secure": true,
         "changeOrigin": true,
         "logLevel": "debug"
