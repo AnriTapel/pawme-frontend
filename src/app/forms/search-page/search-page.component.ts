da3cd9c790f453233fe096093b2cd127e71db138
shumik.vladyslav <shumik.vladyslav@gmail.com>
2019-12-25 18:19:44 +0200

    search city, breed. Multiselect

--- src/app/forms/search-page/search-page.component.ts
+++ src/app/forms/search-page/search-page.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit, HostListener } from '@angular/core';
 import { AppService } from 'src/app/services/app-service/app.service';
-import { SearchControllerService } from 'src/app/api/api';
+import { SearchControllerService, BreederControllerService } from 'src/app/api/api';
 import { SearchTerms } from '../../model/searchTerms';
 import { SearchMeta } from '../../model/searchMeta';
 import { PetSelectionRequest } from '../../model/petSelectionRequest';
 import { BreederSearchEntry } from '../../model/breederSearchEntry';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 
 
@@ -21,7 +21,13 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class SearchPageComponent implements OnInit {
 
-  constructor(public appService: AppService, private searchControllerService: SearchControllerService, private route: ActivatedRoute) { }
+
+  constructor(
+    public appService: AppService,
+    private searchControllerService: SearchControllerService,
+    private route: ActivatedRoute,
+    private breederService: BreederControllerService
+  ) { }
 
   searchData: SearchTerms = {
     breed: null,
@@ -41,13 +47,13 @@ export class SearchPageComponent implements OnInit {
     id: null
   };
 
-  getSearchData: any = null; 
+  getSearchData: any = null;
   getMetaSearchData: SearchMeta = null;//BreederSearchEntry = null;
   //getMetaSearchData: SearchMeta = null;
   lenghtSearchData: number;
   showTableData: boolean = false;
   p: any;
-  userFilter: any ;
+  userFilter: any;
   // dataFilter: any ;
   invalidFields: any[] = [];
   showBox: boolean = false;
@@ -56,53 +62,81 @@ export class SearchPageComponent implements OnInit {
   // scrWidth: any;
   state: boolean;
   breed: string[];
-  
+
+  breedList;
+  citiesList;
 
   ngOnInit() {
 
-  this.route.queryParamMap.subscribe(params => this.breed = params.getAll('breed'));
-  if (this.breed!== null) {
-    this.searchData.breed = Number(this.breed);
+    this.route.queryParamMap.subscribe(params => this.breed = params.getAll('breed'));
+    if (this.breed !== null) {
+      this.searchData.breed = Number(this.breed);
+    }
+    this.searchControllerService.getSearchMetaUsingGET()
+      .subscribe((res) => {
+        this.getMetaSearchData = <SearchMeta>res;
+        
+        this.breedList = this.appService.breeds;
+        this.breedList.forEach((item) => {
+          item.disabled = true;
+          this.getMetaSearchData['breeds'].forEach(element => {
+            if (+item.id === +element) {
+              item.disabled = false;
+            }
+          });
+        });
+
+        this.citiesList = [];
+        this.appService.cities.forEach((item, index) => {
+          this.citiesList.push(
+            {
+              name: item,
+              disabled: true
+            }
+          )
+          this.getMetaSearchData['cities'].forEach(element => {
+            if (item === element) {
+              this.citiesList[index].disabled = false;
+            }
+          });
+        });
+
+      }, (err) => {
+        if (err.status == 404)
+          console.log('error', err);
+      });
+    this.searchControllerService.findUsingPOST(this.searchData)
+      .subscribe((res) => {
+        this.lenghtSearchData = res.length;
+        this.getSearchData = <any>res;
+      }, (err) => {
+        if (err.status == 404)
+          console.log('error', err)
+      });
   }
-  this.searchControllerService.getSearchMetaUsingGET()
-  .subscribe((res) => {
-      this.getMetaSearchData = <SearchMeta>res;
-  }, (err) => {
-      if (err.status == 404)
-      console.log('error', err);
-  });
-   this.searchControllerService.findUsingPOST(this.searchData)
-            .subscribe((res) => {
-              this.lenghtSearchData = res.length;
-                this.getSearchData = <any>res;
-            }, (err) => {
-                if (err.status == 404)
-                console.log('error', err) 
-            });       
+
+
+  @HostListener('window:scroll', ['$event'])
+  getScreenSize(event) {
+
+    const scrollPosition = window.pageYOffset;
+    if (scrollPosition > 400) {
+      this.state = true;
+    } else {
+      this.state = false;
+    }
+    //console.log("Scroll Event", window.pageYOffset );
   }
-
-
-@HostListener('window:scroll', ['$event']) 
-getScreenSize(event) {
-
-  const scrollPosition = window.pageYOffset;
-  if (scrollPosition > 400) {
-    this.state = true;
-  } else {
-    this.state = false;
-  } 
-  //console.log("Scroll Event", window.pageYOffset );
-}
   public changeInput(event) {
     console.log('this.searchData', this.searchData);
-     this.searchControllerService.findUsingPOST(this.searchData)
-            .subscribe((res) => {
-                this.lenghtSearchData = res.length;
-                this.getSearchData = <any>res;
-            }, (err) => {
-                if (err.status == 404)
-                console.log('error', err)
-            });
+    this.searchControllerService.findUsingPOST(this.searchData)
+      .subscribe((res) => {
+        this.lenghtSearchData = res.length;
+        this.getSearchData = <any>res;
+      }, (err) => {
+        if (err.status == 404)
+          console.log('error', err)
+      });
   }
   public updateSearch($event) {
     //console.log('$event.target.value', $event);
@@ -113,7 +147,7 @@ getScreenSize(event) {
       this.showTableData = true;
     }
 
-  } 
+  }
   fieldEdited(field: string): void {
     this.invalidFields = this.invalidFields.filter(it => it != field);
   }
@@ -133,12 +167,12 @@ getScreenSize(event) {
       return;
     this.sendEmaillData.email = this.sendEmaillData.email.toLowerCase();
     this.searchControllerService.savePetSelectionRequestUsingPOST(this.sendEmaillData)
-    .subscribe((res) => {
-      this.showBox = true;
-    }, (err) => {
+      .subscribe((res) => {
+        this.showBox = true;
+      }, (err) => {
         if (err.status == 404)
-        console.log('error', err)
-    });
+          console.log('error', err)
+      });
 
   }
 
