b0db890a7ad4874d5352bf7914899d16fe95ba26
ani <tonoyan.ani@gmail.com>
2020-01-30 20:08:50 +0400

    Прописать, что заводчик не может начать чат с заводчиком

--- src/app/client-registration/client-registration.component.ts
+++ src/app/client-registration/client-registration.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { PopupTemplateService } from '../services/popup-service/popup-template.service';
+import { BreederControllerService } from 'src/app/api/api';
+import { AppService } from 'src/app/services/app-service/app.service';
+import { NotificationBarService } from 'src/app/services/nofitication-service/notification-bar.service';
+
+
+
 
 
 
@@ -10,14 +16,27 @@ import { PopupTemplateService } from '../services/popup-service/popup-template.s
 })
 export class ClientRegistrationComponent implements OnInit {
 
-  constructor(private popupService: PopupTemplateService) { }
+  constructor(
+    private popupService: PopupTemplateService,
+    private breederService: BreederControllerService,
+    public appService: AppService,
+    private notificationService: NotificationBarService) { }
 
   ngOnInit() {
+    this.breederService.meUsingGET().subscribe((me) => {
+      this.appService.meData = me;
+    });
   }
 
   showBreederMessagePopup(): void {
-    this.popupService.setCurrentForm('client-chat');
-    this.popupService.setShowStatus(true);
+    if (this.appService.meData.type == 'BREEDER') {
+      this.notificationService.setContext('Пожалуйста, выйдите из аккаунта заводчика', false);
+      this.notificationService.setVisibility(true);
+    } else {
+      this.popupService.setCurrentForm('client-chat');
+      this.popupService.setShowStatus(true);
+    }
+
   }
 
 }
